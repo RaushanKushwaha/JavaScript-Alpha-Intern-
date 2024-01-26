1bd91426262c2be1891beefd73cdd1bb9f4ff8e0
Raushan kushwaha <raushankushawaha18@gmail.com>
2024-01-26 22:20:49 +0530

    Practice Week Day-3

---
+++ AIC-JavaScript/WEEK-1/Practice week-2/QuestionOnVariables/evenOdd.js
@@ -0,0 +1,6 @@
+const num = 10;
+if (num%2===0) {
+    console.log("Even number");
+}else{
+    console.log("Odd number");
+}
---
+++ AIC-JavaScript/WEEK-1/Practice week-2/QuestionOnVariables/largestNum.js
@@ -0,0 +1,15 @@
+const x =100;
+const y =20;
+const z =130;
+const k =30;
+
+if (x>y && x>z && x>k) {
+    console.log(x+ " is largest");
+}else if (y>x && y>z && y>k) {
+    console.log(y+ " is largest"); 
+} else if(z>x && z>y && z>k){
+    console.log(z+ " is largest");
+}else{
+    console.log(k +" is largest");
+}
+    
